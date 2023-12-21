442ca966dd377a89f1337a7d66a1f1ed839ffb2a
Ian Halverson <ihalverson@burrislogistics.com>
2023-12-21 14:02:17 -0500

    add configs to skip threads, match on sender or subject only, and blacklist pattern matching. Popup styling

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 let intervalId = null;
 
+function escapeRegex(string) {
+  return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, "\\$&");
+}
+
 function consolidateMessages() {
   if (chrome && chrome.runtime && chrome.runtime.id) {
     chrome.storage.sync.get(
@@ -7,17 +11,25 @@ function consolidateMessages() {
         enabled: true, // Default to enabled
         leaveGrayBorders: true, // Default to enabled
         onlyAdjacent: false, // Default to disabled
+        excludeThreads: false, // Default to disabled
         groupedNumberTemplate: "({{n}}) -",
+        matchOn: "Sender & Subject",
         senderList: "",
         subjectList: "",
+        senderBlacklist: "",
+        subjectBlacklist: "",
       },
       function ({
         enabled,
         leaveGrayBorders,
         onlyAdjacent,
+        excludeThreads,
         groupedNumberTemplate,
+        matchOn,
         senderList,
         subjectList,
+        senderBlacklist,
+        subjectBlacklist,
       }) {
         if (!enabled) return;
         const requiredSenders = senderList
@@ -25,6 +37,14 @@ function consolidateMessages() {
           .map((it) => it.trim().toLowerCase())
           .filter(Boolean);
         const requiredSubjects = subjectList
+          .split(",")
+          .map((it) => it.trim().toLowerCase())
+          .filter(Boolean);
+        const blacklistedSenders = senderBlacklist
+          .split(",")
+          .map((it) => it.trim().toLowerCase())
+          .filter(Boolean);
+        const blacklistedSubjects = subjectBlacklist
           .split(",")
           .map((it) => it.trim().toLowerCase())
           .filter(Boolean);
@@ -44,124 +64,205 @@ function consolidateMessages() {
           );
           const hasThreadButton =
             threadButton && (threadButton.textContent || "").length === 1; //chevron symbol
-          const senderDiv = div.querySelector(
-            `div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(${
-              hasThreadButton ? 3 : 2
-            }) > span`
-          );
-          const sender = senderDiv ? senderDiv.textContent.trim() : "";
-          const subjDiv = div.querySelector(
-            `div > div:nth-child(2) > div > div:nth-child(2) > div > div > span`
-          );
-          const subj = subjDiv ? subjDiv.textContent.trim() : "";
+          if (!(excludeThreads && hasThreadButton)) {
+            const senderDiv = div.querySelector(
+              `div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(${
+                hasThreadButton ? 3 : 2
+              }) > span`
+            );
+            const sender = senderDiv ? senderDiv.textContent.trim() : "";
+            const subjDiv = div.querySelector(
+              `div > div:nth-child(2) > div > div:nth-child(2) > div > div > span`
+            );
+            const subj = subjDiv ? subjDiv.textContent.trim() : "";
 
-          let nextSender;
-          let nextSendersList;
-          let nextSubj;
-          const updateNextMessageToCheckVars = () => {
-            if (nextMessageToCheck) {
-              const nextThreadButton = nextMessageToCheck.querySelector(
-                `div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(1) > button`
-              );
-              const nextHasThreadButton =
-                nextThreadButton &&
-                (nextThreadButton.textContent || "").length === 1; //chevron symbol
-              const nextSenderDiv = nextMessageToCheck.querySelector(
-                `div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(${
-                  nextHasThreadButton ? 3 : 2
-                }) > span`
-              );
-              nextSender = nextSenderDiv
-                ? nextSenderDiv.textContent.trim()
-                : "";
-              nextSendersList = nextSender
-                .split(";")
-                .map((it) => it.trim().toLowerCase())
-                .filter(Boolean);
-              const nextSubjDiv = nextMessageToCheck.querySelector(
-                `div > div:nth-child(2) > div > div:nth-child(2) > div > div > span`
-              );
-              nextSubj = nextSubjDiv ? nextSubjDiv.textContent.trim() : "";
-            }
-          };
-          updateNextMessageToCheckVars();
+            let nextSender;
+            let nextSendersList;
+            let nextSubj;
+            let nextHasThreadButton;
+            const updateNextMessageToCheckVars = () => {
+              if (nextMessageToCheck) {
+                const nextThreadButton = nextMessageToCheck.querySelector(
+                  `div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(1) > button`
+                );
+                nextHasThreadButton =
+                  nextThreadButton &&
+                  (nextThreadButton.textContent || "").length === 1; //chevron symbol
+                const nextSenderDiv = nextMessageToCheck.querySelector(
+                  `div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(${
+                    nextHasThreadButton ? 3 : 2
+                  }) > span`
+                );
+                nextSender = nextSenderDiv
+                  ? nextSenderDiv.textContent.trim()
+                  : "";
+                nextSendersList = nextSender
+                  .split(";")
+                  .map((it) => it.trim().toLowerCase())
+                  .filter(Boolean);
+                const nextSubjDiv = nextMessageToCheck.querySelector(
+                  `div > div:nth-child(2) > div > div:nth-child(2) > div > div > span`
+                );
+                nextSubj = nextSubjDiv ? nextSubjDiv.textContent.trim() : "";
+              }
+            };
+            updateNextMessageToCheckVars();
 
-          _whileLoop: while (nextMessageToCheck) {
-            iteration++;
-            if (
-              sender &&
-              subj &&
-              nextSender &&
-              nextSubj &&
-              sender === nextSender &&
-              subj === nextSubj &&
-              count <= limit &&
-              (!requiredSenders.length ||
-                nextSendersList.some((sender) =>
-                  requiredSenders.some((requiredSender) => {
-                    const isStart = requiredSender.endsWith("%");
-                    const isEnd = requiredSender.startsWith("%");
+            _whileLoop: while (nextMessageToCheck) {
+              iteration++;
+              if (
+                sender &&
+                subj &&
+                nextSender &&
+                nextSubj &&
+                (matchOn === "Subject Only" || sender === nextSender) &&
+                (matchOn === "Sender Only" || subj === nextSubj) &&
+                count <= limit &&
+                !(excludeThreads && nextHasThreadButton) &&
+                (!requiredSenders.length ||
+                  nextSendersList.some((sender) =>
+                    requiredSenders.some((requiredSender) => {
+                      const isStart = requiredSender.endsWith("%");
+                      const isEnd = requiredSender.startsWith("%");
+                      if (isStart && isEnd) {
+                        const withoutPerc = requiredSender
+                          .replace(/^\%/, "")
+                          .replace(/\%$/, "");
+                        return withoutPerc && sender.includes(withoutPerc);
+                      } else if (isStart) {
+                        return sender.startsWith(
+                          requiredSender.replace(/\%$/, "")
+                        );
+                      } else if (isEnd) {
+                        return sender.endsWith(
+                          requiredSender.replace(/^\%/, "")
+                        );
+                      } else {
+                        return sender === requiredSender;
+                      }
+                    })
+                  )) &&
+                (!blacklistedSenders.length ||
+                  !nextSendersList.some((sender) =>
+                    blacklistedSenders.some((blacklistedSender) => {
+                      const isStart = blacklistedSender.endsWith("%");
+                      const isEnd = blacklistedSender.startsWith("%");
+                      if (isStart && isEnd) {
+                        const withoutPerc = blacklistedSender
+                          .replace(/^\%/, "")
+                          .replace(/\%$/, "");
+                        return withoutPerc && sender.includes(withoutPerc);
+                      } else if (isStart) {
+                        return sender.startsWith(
+                          blacklistedSender.replace(/\%$/, "")
+                        );
+                      } else if (isEnd) {
+                        return sender.endsWith(
+                          blacklistedSender.replace(/^\%/, "")
+                        );
+                      } else {
+                        return sender === blacklistedSender;
+                      }
+                    })
+                  )) &&
+                (!requiredSubjects.length ||
+                  requiredSubjects.some((requiredSubject) => {
+                    const isStart = requiredSubject.endsWith("%");
+                    const isEnd = requiredSubject.startsWith("%");
                     if (isStart && isEnd) {
-                      const withoutPerc = requiredSender
+                      const withoutPerc = requiredSubject
                         .replace(/^\%/, "")
                         .replace(/\%$/, "");
-                      return withoutPerc && sender.includes(withoutPerc);
-                    } else if (isStart) {
-                      return sender.startsWith(
-                        requiredSender.replace(/\%$/, "")
+                      return (
+                        withoutPerc &&
+                        nextSubj.toLowerCase().includes(withoutPerc)
                       );
+                    } else if (isStart) {
+                      return nextSubj
+                        .toLowerCase()
+                        .startsWith(requiredSubject.replace(/\%$/, ""));
                     } else if (isEnd) {
-                      return sender.endsWith(requiredSender.replace(/^\%/, ""));
+                      return nextSubj
+                        .toLowerCase()
+                        .endsWith(requiredSubject.replace(/^\%/, ""));
                     } else {
-                      return sender === requiredSender;
+                      return nextSubj.toLowerCase() === requiredSubject;
                     }
-                  })
-                )) &&
-              (!requiredSubjects.length ||
-                requiredSubjects.some((requiredSubject) => {
-                  const isStart = requiredSubject.endsWith("%");
-                  const isEnd = requiredSubject.startsWith("%");
-                  if (isStart && isEnd) {
-                    const withoutPerc = requiredSubject
-                      .replace(/^\%/, "")
-                      .replace(/\%$/, "");
-                    return (
-                      withoutPerc &&
-                      nextSubj.toLowerCase().includes(withoutPerc)
-                    );
-                  } else if (isStart) {
-                    return nextSubj
-                      .toLowerCase()
-                      .startsWith(requiredSubject.replace(/\%$/, ""));
-                  } else if (isEnd) {
-                    return nextSubj
-                      .toLowerCase()
-                      .endsWith(requiredSubject.replace(/^\%/, ""));
-                  } else {
-                    return nextSubj.toLowerCase() === requiredSubject;
-                  }
-                }))
-            ) {
-              count++;
-              const toRemove = leaveGrayBorders
-                ? nextMessageToCheck
-                : nextMessageToCheck.parentNode.parentNode.parentNode;
-              toRemoves.push(toRemove);
-            } else {
-              if (onlyAdjacent) {
-                break _whileLoop;
+                  })) &&
+                (!blacklistedSubjects.length ||
+                  !blacklistedSubjects.some((blacklistedSubject) => {
+                    const isStart = blacklistedSubject.endsWith("%");
+                    const isEnd = blacklistedSubject.startsWith("%");
+                    if (isStart && isEnd) {
+                      const withoutPerc = blacklistedSubject
+                        .replace(/^\%/, "")
+                        .replace(/\%$/, "");
+                      return (
+                        withoutPerc &&
+                        nextSubj.toLowerCase().includes(withoutPerc)
+                      );
+                    } else if (isStart) {
+                      return nextSubj
+                        .toLowerCase()
+                        .startsWith(blacklistedSubject.replace(/\%$/, ""));
+                    } else if (isEnd) {
+                      return nextSubj
+                        .toLowerCase()
+                        .endsWith(blacklistedSubject.replace(/^\%/, ""));
+                    } else {
+                      return nextSubj.toLowerCase() === blacklistedSubject;
+                    }
+                  }))
+              ) {
+                count++;
+                const toRemove = leaveGrayBorders
+                  ? nextMessageToCheck
+                  : nextMessageToCheck.parentNode.parentNode.parentNode;
+                toRemoves.push(toRemove);
+              } else {
+                if (onlyAdjacent) {
+                  break _whileLoop;
+                }
               }
+              nextMessageToCheck = allDivs[ix + iteration];
+              updateNextMessageToCheckVars();
             }
-            nextMessageToCheck = allDivs[ix + iteration];
-            updateNextMessageToCheckVars();
-          }
 
-          if (count > 1) {
-            if (subj) {
-              subjDiv.textContent = `${groupedNumberTemplate.replace(
-                "{{n}}",
-                count
-              )} ${subj}`;
+            if (count > 1) {
+              if (subj) {
+                const regexForAlreadyPrepended = new RegExp(
+                  "^" +
+                    escapeRegex(groupedNumberTemplate).replace(
+                      "\\{\\{n\\}\\}",
+                      "[0-9]+"
+                    )
+                );
+                if (
+                  subjDiv.textContent &&
+                  subjDiv.textContent.match(regexForAlreadyPrepended)
+                ) {
+                  subjDiv.textContent = subjDiv.textContent.replace(
+                    regexForAlreadyPrepended,
+                    groupedNumberTemplate.replace("{{n}}", count)
+                  );
+                } else {
+                  const newSubj =
+                    matchOn === "Sender Only"
+                      ? subj.split("").slice(0, 70).join("") +
+                        ". . . (SUBJECT(S) VARIOUS/GROUPED)"
+                      : subj;
+                  subjDiv.textContent = `${groupedNumberTemplate.replace(
+                    "{{n}}",
+                    count
+                  )} ${newSubj}`;
+                  if (matchOn === "Subject Only") {
+                    senderDiv.textContent = `${sender
+                      .split("")
+                      .slice(0, 23)
+                      .join("")} (VARIOUS/GROUPED)`;
+                  }
+                }
+              }
             }
           }
         });
--- popup.js
+++ popup.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const enabled = document.getElementById("ogeEnabled");
   const leaveGrayBorders = document.getElementById("ogeLeaveGrayBorders");
   const onlyAdjacent = document.getElementById("ogeOnlyAdjacent");
+  const excludeThreads = document.getElementById("ogeExcludeThreads");
   const groupedNumberTemplate = document.getElementById(
     "ogeGroupedNumberTemplate"
   );
   const runHowOften = document.getElementById("ogeRunHowOften");
+  const matchOn = document.getElementById("ogeMatchOn");
   const senderList = document.getElementById("ogeSenderList");
   const subjectList = document.getElementById("ogeSubjectList");
+  const senderBlacklist = document.getElementById("ogeSenderBlacklist");
+  const subjectBlacklist = document.getElementById("ogeSubjectBlacklist");
 
   // Load stored states or default to true or false
   chrome.storage.sync.get(
@@ -15,19 +19,27 @@ document.addEventListener("DOMContentLoaded", function () {
       enabled: true, // Default to enabled
       leaveGrayBorders: true, // Default to enabled
       onlyAdjacent: false, // Default to disabled
+      excludeThreads: false, // Default to disabled
       groupedNumberTemplate: "({{n}}) -",
       runHowOften: 2,
+      matchOn: "Sender & Subject",
       senderList: "",
       subjectList: "",
+      senderBlacklist: "",
+      subjectBlacklist: "",
     },
     function (items) {
       enabled.checked = items.enabled;
       leaveGrayBorders.checked = items.leaveGrayBorders;
       onlyAdjacent.checked = items.onlyAdjacent;
+      excludeThreads.checked = items.excludeThreads;
       groupedNumberTemplate.value = items.groupedNumberTemplate;
       runHowOften.value = items.runHowOften;
+      matchOn.value = items.matchOn;
       senderList.value = items.senderList;
       subjectList.value = items.subjectList;
+      senderBlacklist.value = items.senderBlacklist;
+      subjectBlacklist.value = items.subjectBlacklist;
     }
   );
 
@@ -48,6 +60,9 @@ document.addEventListener("DOMContentLoaded", function () {
   onlyAdjacent.addEventListener("change", function () {
     chrome.storage.sync.set({ onlyAdjacent: onlyAdjacent.checked });
   });
+  excludeThreads.addEventListener("change", function () {
+    chrome.storage.sync.set({ excludeThreads: excludeThreads.checked });
+  });
   groupedNumberTemplate.addEventListener("change", function () {
     chrome.storage.sync.set({
       groupedNumberTemplate: groupedNumberTemplate.value,
@@ -56,10 +71,19 @@ document.addEventListener("DOMContentLoaded", function () {
   runHowOften.addEventListener("change", function () {
     chrome.storage.sync.set({ runHowOften: runHowOften.value });
   });
+  matchOn.addEventListener("change", function () {
+    chrome.storage.sync.set({ matchOn: matchOn.value });
+  });
   senderList.addEventListener("change", function () {
     chrome.storage.sync.set({ senderList: senderList.value });
   });
   subjectList.addEventListener("change", function () {
     chrome.storage.sync.set({ subjectList: subjectList.value });
+  });
+  senderBlacklist.addEventListener("change", function () {
+    chrome.storage.sync.set({ senderBlacklist: senderBlacklist.value });
+  });
+  subjectBlacklist.addEventListener("change", function () {
+    chrome.storage.sync.set({ subjectBlacklist: subjectBlacklist.value });
   });
 });
